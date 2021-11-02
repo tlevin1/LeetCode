ad1ca6e7a81dfb592ea5c4c04d63c20e321a4ba4
tlevin1 <61256186+tlevin1@users.noreply.github.com>
2021-11-01 22:20:16 -0400

    bst to gst (with correct logic but main bug)

--- main.cpp
+++ main.cpp
@@ -1,70 +1,128 @@
+//Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key
+// of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+//As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+//The left subtree of a node contains only nodes with keys less than the node's key.
+//The right subtree of a node contains only nodes with keys greater than the node's key.
+//Both the left and right subtrees must also be binary search trees.
+
+//Example 1:
+//Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+//Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+//Example 2:
+//Input: root = [0,null,1]
+//Output: [1,null,1]
+
+//Example 3:
+//Input: root = [1,0,2]
+//Output: [3,3,2]
+
+//Example 4:
+//Input: root = [3,2,4,1]
+//Output: [7,9,4,10]
+
+//Constraints:
+//The number of nodes in the tree is in the range [1, 100].
+//0 <= Node.val <= 100
+//All the values in the tree are unique.
+//root is guaranteed to be a valid binary search tree.
+
+// * Definition for a binary tree node.
+
+
+
 #include <iostream>
-
-#include<bits/stdc++.h>
-
 using namespace std;
-// Program to check if BST is valid BST
-// BST Properties:
-// left subtree  < root
-// right subtree > root
-// both left and right subtrees must be valid BSTs
 
-// time complexity for this implementation = O(n) (We traverse every node once)
-class node
-{
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
 public:
-    int data;
-    node* left;
-    node* right;
-
-    /* Constructor that allocates
-    a new node with the given data
-    and NULL left and right pointers. */
-    node(int data)
-    {
-        this->data = data;
-        this->left = NULL;
-        this->right = NULL;
+    // method to transform a BST to a GST
+    TreeNode* bstToGst(TreeNode* root) {
+        // traverse BST in reverse - inorder traversal reversed so Right -> Root -> Left
+        // reverse gives the keys in decreasing order
+        // as traverse keep track of all nodes greater to curr and store sum
+        int sum = 0;
+        TreeNode* t = bstHelper(root,sum);
+        return t;
     }
+    TreeNode* bstHelper(TreeNode* curr,int& sum){
+        // base case
+        if(!curr){
+            return curr;
+        }
+
+        // visit right 1st
+        bstHelper(curr -> right,sum);
+        sum = sum + curr -> val;
+        curr -> val = sum;
+        //cout << curr -> val << endl;
+        // visit root 2nd
+        //bstHelper(curr,sum);
+        // visit left last
+        bstHelper(curr -> left,sum);
+        return curr;
+    }
+    void printBst(struct TreeNode* root){
+    // inorder traversal
+    // RLR
+        if(root == NULL) {
+            return;
+        }
+
+        printBst(root -> left);
+        cout << root -> val;
+        cout << " " ;
+        printBst(root -> right);
+        }
+
+    void printBstPre(struct TreeNode* root){
+        // preorder traversal
+        // RLR
+        if(root == NULL) {
+            cout << "null ";
+            return;
+        }
+        cout << root -> val;
+        cout << " " ;
+        printBstPre(root -> left);
+        printBstPre(root -> right);
+    }
+
 };
 
-int isBSTUtil(node* node, int min, int max);
 
-int isBST(node* node)
-{
-    return(isBSTUtil(node, INT_MIN, INT_MAX));
-}
-int isBSTUtil(node* node, int min, int max)
-{
-    // Method:
-    // using BST Util we can traverse tree and keep track of min and max
-    // we can visit every node once
+int main(){
+    //Example 1:
+    //Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+    //Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+    Solution s;
 
-    // empty bst (considered still valid)
-    if(node == NULL){
-        return 1;
-    }
-    // checking min/max constraint for BSTs
-    if(node -> data < min || node -> data > max){
-        // returns false
-        return 0;
-    }
-    // recursive function call to check subtrees recursively
-    return isBSTUtil(node -> left,min,node -> data -1) && isBSTUtil(node -> right,node -> data + 1,max);
+    TreeNode* root = new TreeNode(4);
+    root -> right = new TreeNode(6);
+    root -> left = new TreeNode(1);
+    root -> left -> right = new TreeNode(2);
+    root -> left -> left = new TreeNode(0);
+    root -> right -> right = new TreeNode(7);
+    root -> right -> left = new TreeNode(5);
+    root -> left -> right -> right  = new TreeNode(3);
+    root -> right -> right -> right = new TreeNode(8);
 
-}
-int main() {
-
-    node *root = new node(1);
-    root->left = new node(2);
-    root->right = new node(5);
-    root->left->left = new node(1);
-    root->left->right = new node(3);
-
-    if(isBST(root))
-        cout<<"Is BST";
-    else
-        cout<<"Not a BST";
+    s.printBstPre(root);
+    cout << endl;
+    TreeNode* t = s.bstToGst(root);
+    s.printBstPre(t);
 
     return 0;
 }
