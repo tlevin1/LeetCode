7da1b7d867090310f13b6c608462c952bbb0f046
tlevin1 <61256186+tlevin1@users.noreply.github.com>
2021-11-09 22:31:42 -0500

    reverse subarray

--- reversearr.py
+++ reversearr.py
@@ -2,6 +2,14 @@
 
 def reverse(arr, i, j):
     #assuming i is starting index and reversing upto and including j
+    #edge cases: [], i > j, negative i or j
+    if not arr:
+        return "empty list!"
+    if i > j:
+        return "invalid reverse!"
+    if i < 0 or j < 0:
+        return "invalid indices provided!"
+
     while i <= len(arr) and j >= 0:
         #once at last swap
         if i == j - 1:
@@ -26,5 +34,8 @@ print(reverse([1, 2, 3, 4], 0, 3))
 print(reverse([1, 2, 3, 4], 0, 1))
 print(reverse([1, 2, 3, 4], 0, 0))
 print(reverse([1, 2, 3, 4], 5, 6))
+print(reverse([1, 2, 3, 4], 7, 1))
+print(reverse([1, 2, 3, 4], -1, 1))
+print(reverse([], 0, 1))
 
 
