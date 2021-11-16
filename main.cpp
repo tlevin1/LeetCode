80cc26bfc9ab20d555969c1479b11e520eb9eda6
tlevin1 <61256186+tlevin1@users.noreply.github.com>
2021-11-16 16:22:52 -0500

    remove element from linked list

--- main.cpp
+++ main.cpp
@@ -1,128 +1,82 @@
-//Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key
-// of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
-
-//As a reminder, a binary search tree is a tree that satisfies these constraints:
-
-//The left subtree of a node contains only nodes with keys less than the node's key.
-//The right subtree of a node contains only nodes with keys greater than the node's key.
-//Both the left and right subtrees must also be binary search trees.
-
-//Example 1:
-//Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
-//Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
-
-//Example 2:
-//Input: root = [0,null,1]
-//Output: [1,null,1]
-
-//Example 3:
-//Input: root = [1,0,2]
-//Output: [3,3,2]
-
-//Example 4:
-//Input: root = [3,2,4,1]
-//Output: [7,9,4,10]
-
-//Constraints:
-//The number of nodes in the tree is in the range [1, 100].
-//0 <= Node.val <= 100
-//All the values in the tree are unique.
-//root is guaranteed to be a valid binary search tree.
-
-// * Definition for a binary tree node.
-
-
-
 #include <iostream>
-using namespace std;
+
+/*Given the head of a linked list and an integer val,
+ remove all the nodes of the linked list that has Node.val == val, and return the new head.
+Example 1:
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
 
 
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+ */
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
 public:
-    // method to transform a BST to a GST
-    TreeNode* bstToGst(TreeNode* root) {
-        // traverse BST in reverse - inorder traversal reversed so Right -> Root -> Left
-        // reverse gives the keys in decreasing order
-        // as traverse keep track of all nodes greater to curr and store sum
-        int sum = 0;
-        TreeNode* t = bstHelper(root,sum);
-        return t;
-    }
-    TreeNode* bstHelper(TreeNode* curr,int& sum){
-        // base case
-        if(!curr){
-            return curr;
-        }
+    // time complexity : O(N)
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* curr = head;
+        // sentinal node to ensure list is never empty/headless, does not contain any data
+        ListNode* sentinal = new ListNode(0);
+        sentinal -> next = head;
+        ListNode *prev = sentinal;
+        // node to delete
+        ListNode *temp = nullptr;
 
-        // visit right 1st
-        bstHelper(curr -> right,sum);
-        sum = sum + curr -> val;
-        curr -> val = sum;
-        //cout << curr -> val << endl;
-        // visit root 2nd
-        //bstHelper(curr,sum);
-        // visit left last
-        bstHelper(curr -> left,sum);
-        return curr;
-    }
-    void printBst(struct TreeNode* root){
-    // inorder traversal
-    // RLR
-        if(root == NULL) {
-            return;
+        //traverse list to find element to delete
+        while(curr != nullptr) {
+            if (curr->val == val) {
+                // update next
+                prev->next = curr->next;
+                temp = curr;
+            } else {
+                prev = curr;
+            }
+            curr = curr->next;
+            if (temp != nullptr) {
+                delete temp;
+                temp = nullptr;
+            }
         }
-
-        printBst(root -> left);
-        cout << root -> val;
-        cout << " " ;
-        printBst(root -> right);
+            // return head of modified list
+            ListNode *ret = sentinal -> next;
+            delete sentinal;
+            return ret;
         }
-
-    void printBstPre(struct TreeNode* root){
-        // preorder traversal
-        // RLR
-        if(root == NULL) {
-            cout << "null ";
-            return;
-        }
-        cout << root -> val;
-        cout << " " ;
-        printBstPre(root -> left);
-        printBstPre(root -> right);
-    }
-
 };
 
-
-int main(){
-    //Example 1:
-    //Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
-    //Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+int main() {
+    //Input: head = [1,2,6,3,4,5,6], val = 6
+    //Output: [1,2,3,4,5]
     Solution s;
-
-    TreeNode* root = new TreeNode(4);
-    root -> right = new TreeNode(6);
-    root -> left = new TreeNode(1);
-    root -> left -> right = new TreeNode(2);
-    root -> left -> left = new TreeNode(0);
-    root -> right -> right = new TreeNode(7);
-    root -> right -> left = new TreeNode(5);
-    root -> left -> right -> right  = new TreeNode(3);
-    root -> right -> right -> right = new TreeNode(8);
-
-    s.printBstPre(root);
-    cout << endl;
-    TreeNode* t = s.bstToGst(root);
-    s.printBstPre(t);
-
+    ListNode *head = new ListNode(1);
+    std::cout << head -> val << std::endl;
+    head -> next = new ListNode(2);
+    head = s.removeElements(head,1);
+    std::cout << head -> val << std::endl;
     return 0;
 }
+
+
+
